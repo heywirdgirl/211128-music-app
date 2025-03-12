202ba934d09cbdf766316e887838a78045689590
heywirdgirl <awsmydream@gmail.com>
2025-03-13 02:50:08 +0700

    0249

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,6 @@ import 'tailwindcss/tailwind.css'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      		<div className="hidden sm:block flex-none w-64 h-96 rounded-lg border-2 border-indigo-500">ads test</div>
-      		<div className="flex-auto w-max">	
       			<Component {...pageProps} />
       		</div>
     </Layout>
