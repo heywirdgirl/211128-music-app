81aec8501d4868577d2dbaa0f7d67b39be7ee78d
heywirdgirl <awsmydream@gmail.com>
2025-03-13 16:42:02 +0700

    441

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,16 @@ export default function Home({ products }) {
   return (
     <>
       <Header />
+      
+      <div
+  className="relative min-h-screen bg-cover bg-center bg-no-repeat lg:bg-fixed"
+  style={{ backgroundImage: "url('/images/hat.jpg')" }}
+>
+  {/* DARK OVERLAY */}
+  <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+
+
+      
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold text-center my-6">Simple Shop</h1>
         
@@ -24,6 +34,7 @@ export default function Home({ products }) {
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
+      </div>
       </div>
     </>
   );
